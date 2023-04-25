7e0a64735016d21806f3af434b27b955026612ec
hussein.youssef <hussein.youssef@inmobiles.net>
2023-04-25 23:26:26 +0300

    add some flow for unit testing

--- Cms.UnitTest/Apis/Calls.cs
+++ Cms.UnitTest/Apis/Calls.cs
@@ -17,7 +17,7 @@ namespace Cms.UnitTest.Apis
         {
             ListBreweriesResponse apiresp = new ListBreweriesResponse();
             string host = cmsHost;
-            string uri = string.Concat("http://", host, "/api/Brewery/ListBreweries");
+            string uri = string.Concat("https://", host, "/api/Brewery/ListBreweries");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
             request.ContentType = "application/json";
             request.Method = "GET"; 
@@ -27,6 +27,47 @@ namespace Cms.UnitTest.Apis
                 string result = streamReader.ReadToEnd();
                 apiresp = JsonConvert.DeserializeObject<ListBreweriesResponse>(result);
             }
+
+            return apiresp;
+        }
+        public ListBeersResponse CallApiListBeers(Guid IdBrewery)
+        {
+            ListBeersResponse apiresp = new ListBeersResponse();
+            string host = cmsHost;
+            string uri = string.Concat("https://", host, $"/api/Brewery/ListBeers?IdBrewery={IdBrewery}&page=0&pagesize=10");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.ContentType = "application/json";
+            request.Method = "GET";
+            HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
+            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                apiresp = JsonConvert.DeserializeObject<ListBeersResponse>(result);
+            }
+
+            return apiresp;
+        }
+        public AddBeerResponse CallApiAddBeer(AddBeerRequest addBeerRequest)
+        {
+            AddBeerResponse apiresp = new AddBeerResponse();
+            string host = cmsHost;
+            string uri = string.Concat("https://", host, $"/api/Brewery/AddBeer");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            string json = JsonConvert.SerializeObject(addBeerRequest);
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+            }
+
+            HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
+            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                apiresp = JsonConvert.DeserializeObject<AddBeerResponse>(result);
+            }
+
 
             return apiresp;
         }
--- Cms.UnitTest/UnitTest1.cs
+++ Cms.UnitTest/UnitTest1.cs
@@ -1,6 +1,8 @@
+using Bogus;
 using BreweryWholesaleManagement.Models.Cms.Brewery;
 using Cms.UnitTest.Apis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Cms.UnitTest
 {
@@ -12,13 +14,24 @@ namespace Cms.UnitTest
         {
             Calls calls = new Calls();
             var responseListBreweries = calls.CallApiListBreweries();
+            Guid idBrewery = responseListBreweries.Breweries[0].Id;
             //////use idbrewery from response to add beer
-            AddBeerResponse addBeerResponse = new AddBeerResponse();
-            Assert.AreEqual("Success",addBeerResponse.StatusCode.message);
+            var resplistbeersBeforeadd = calls.CallApiListBeers(idBrewery);
+            ///////
+            var fakerAdd = new Faker<AddBeerRequest>()
+      .RuleFor(u => u.AlcoholContent, f => 10)
+      .RuleFor(u => u.IdBrewery, f => idBrewery)
+      .RuleFor(u => u.Name, f => f.Random.Word())
+      .RuleFor(u => u.Price, f => 20);
+            var generatedRequestAdd = fakerAdd.Generate();
+            var addBeerResponse = calls.CallApiAddBeer(generatedRequestAdd);
+            var resplistbeersAfteradd = calls.CallApiListBeers(idBrewery);
 
-            ////////Check count of beers before add then after add to make sure its added +1
+            Assert.AreEqual("Success", addBeerResponse.StatusCode.message);
+            Assert.AreEqual(resplistbeersBeforeadd.Beers.Count + 1, resplistbeersAfteradd.Beers.Count);
+
             ///retreive fields by id to check if same values
-            ///
+
 
 
         }
